84328fbe983c22ad699e9fdb6a9069648ea5e34d
Hiroto Shioi <shioihigg@gmail.com>
2026-01-04 11:49:31 +0900

    remove msw

--- src/test/setup.browser.ts
+++ src/test/setup.browser.ts
@@ -1,17 +1,7 @@
 import { cleanup } from "@testing-library/react";
-import { afterAll, afterEach, beforeAll } from "vitest";
-import { worker } from "./msw/browser";
+import { afterEach } from "vitest";
 
 // Cleanup after each test
 afterEach(async () => {
-  worker.resetHandlers();
-  cleanup();
-});
-
-beforeAll(async () => {
-  await worker.start({ onUnhandledRequest: "bypass", quiet: true });
-});
-
-afterAll(() => {
-  worker.stop();
+	cleanup();
 });
